52e71259113565ae8cc85ca806bfecb964c8a590
Janne Järvikylä <janne.jarvikyla@gmail.com>
2021-04-16 17:30:46 +0300

    Implemented GeneratorID validation.

--- GeneratorString.cs
+++ GeneratorString.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ExtTDG.Data;
+using System;
 using System.Collections.Generic;
+using System.Numerics;
 
 namespace ExtTDG
 {
@@ -11,54 +13,18 @@ namespace ExtTDG
         private int maxLength;
         private bool hasAnomalies;
         private bool uniqueStrings;
+        bool isMinLengthOk;
+        bool isMaxLengthOk;
 
         public GeneratorString(string allowedChars, string anomalyChars,
                 string minValue, string maxValue, bool hasAnomalies, bool isUnique)
         {
-            if (!String.IsNullOrWhiteSpace(allowedChars))
-            {
-                this.allowedChars = allowedChars;
-            }
-            if (!String.IsNullOrWhiteSpace(anomalyChars))
-            {
-                this.anomalyChars = anomalyChars;
-            }
-            for (int i = 0; i < this.allowedChars.Length; i++)
-            {
-                this.anomalyChars = this.anomalyChars.Replace(this.allowedChars[i].ToString(), "");
-            }
-            if (this.anomalyChars.Length == 0)
-            {
-                this.anomalyChars = "*";
-            }
-            try {
-                this.minLength = Int32.Parse(minValue);
-                if (this.minLength <= 0)
-                {
-                    this.minLength = 1;
-                }
-            } catch (Exception e)
-            {
-                this.minLength = 1;
-            }
-            try
-            {
-                this.maxLength = Int32.Parse(maxValue);
-                if (this.maxLength < this.minLength)
-                {
-                    if (this.minLength >= 10)
-                    {
-                        this.maxLength = this.minLength;
-                    } else
-                    {
-                        this.maxLength = 10;
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                this.maxLength = 10;
-            }
+            this.allowedChars = allowedChars;
+            this.anomalyChars = anomalyChars;
+
+            this.isMinLengthOk = int.TryParse(minValue, out this.minLength);
+            this.isMaxLengthOk = int.TryParse(maxValue, out this.maxLength);
+
             this.hasAnomalies = hasAnomalies;
             this.uniqueStrings = isUnique;
         }
@@ -68,8 +34,91 @@ namespace ExtTDG
         {
             bool isValid = true;
             result = new ValidationResult();
+
+            if (allowedChars == null || allowedChars.Length == 0)
+            {
+                result.messages.Add(ErrorText.kErrAllowedCharsEmpty);
+                isValid = false;
+            }
+
+            if (anomalyChars == null || anomalyChars.Length == 0)
+            {
+                result.messages.Add(ErrorText.kErrAnomalyCharsEmpty);
+                isValid = false;
+            }
+
+            if(isValid)
+            {
+                if(!CheckCharactersIntersection(this.allowedChars, this.anomalyChars))
+                {
+                    result.messages.Add(ErrorText.kErrSharedCharacters);
+                    isValid = false;
+                }
+            }
+
+            // Validate min length
+            if (!isMinLengthOk)
+            {
+                result.messages.Add(ErrorText.kErrParseMinLen);
+                isValid = false;
+            }
+
+            // Validate max length
+            if (!isMaxLengthOk)
+            {
+                result.messages.Add(ErrorText.kErrParseMaxLen);
+                isValid = false;
+            }
+
+            // Validate min/max swap
+            if (isValid)
+            {
+                if (this.minLength >= this.maxLength)
+                {
+                    result.messages.Add(ErrorText.kErrMinGEMaxLen);
+                    isValid = false;
+                }
+
+                if (this.maxLength <= this.minLength)
+                {
+                    result.messages.Add(ErrorText.kErrMaxLEMinLen);
+                    isValid = false;
+                }
+            }
+
+            string uniqueAllowedChars = GetUniqueAllowedChars(this.allowedChars);
+            BigInteger numUniqueCharacters = new BigInteger(uniqueAllowedChars.Length);
+            BigInteger numPossibilities = System.Numerics.BigInteger.Pow(numUniqueCharacters, this.maxLength);
+            BigInteger biNumItems = new BigInteger(numItems);
+            if (biNumItems > numPossibilities)
+            {
+                result.messages.Add(ErrorText.kErrNoUniqueGuaranteeExpandRange);
+                isValid = false;
+            }
+
             result.isValid = isValid;
             return result.isValid;
+        }
+        private bool CheckCharactersIntersection(string s1, string s2)
+        {
+            HashSet<char> set1 = new HashSet<char>();
+            HashSet<char> set2 = new HashSet<char>();
+
+            foreach (char c in s1)
+                set1.Add(c);
+
+            foreach (char c in s2)
+                set2.Add(c);
+
+            foreach (char c in set1)
+            {
+                if (set2.Contains(c))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         public List<string> Generate(int numItems, double anomalyChance, Random rng)
@@ -81,21 +130,6 @@ namespace ExtTDG
             if (!this.hasAnomalies)
             {
                 anomalyProb = 0;
-            }
-
-            // Revert to default allowed chars if trying to generate too much unique Strings with to few chars.
-            if (this.uniqueStrings && this.allowedChars.Length < 10 && numItems > 1000000)
-            {
-                this.allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-                this.anomalyChars = "!#¤%&()=?/;.:,_-<>|@£${[]}*";
-                Console.WriteLine("Too few allowed characters and large amount of unique Strings. Reverting to default allowed characters.");
-            }
-
-            // Increase max length if trying to generate too many unique but short Strings
-            if (Math.Log(2 * numItems, this.allowedChars.Length) > this.maxLength)
-            { 
-                this.maxLength = 2 * (int)Math.Log(2 * numItems, this.allowedChars.Length);
-                Console.WriteLine("Too much unique but short Strings. Maximum length of the Strings increased to " + this.maxLength.ToString());
             }
 
             string str;
@@ -150,6 +184,29 @@ namespace ExtTDG
             }
 
             return str;
+        }
+
+        private string GetUniqueAllowedChars(string str)
+        {
+            HashSet<char> lookUpTable = new HashSet<char>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                char c = str[i];
+                if (!lookUpTable.Contains(c))
+                {
+                    lookUpTable.Add(c);
+                }
+            }
+
+            int index = 0;
+            char[] uniqueChars = new char[lookUpTable.Count];
+            foreach (char c in lookUpTable)
+            {
+                uniqueChars[index] = c;
+                index++;
+            }
+
+            return new string(uniqueChars);
         }
     }
 }
