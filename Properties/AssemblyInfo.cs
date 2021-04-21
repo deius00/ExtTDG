ca58843b285aa0d612c50cc86255a4f863a8a1a2
Janne Järvikylä <janne.jarvikyla@gmail.com>
2021-04-21 11:06:06 +0300

    Fixed correct default values for generators.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ExtTDG")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Ohjelmistotuotanto II - R9")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ExtTDG")]
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
